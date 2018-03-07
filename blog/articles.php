189bc7db538e1262b535b3d1a93639a4d5470cf6
naelait <33450816+naelait@users.noreply.github.com>
2018-03-07 13:56:37 +0100

    function to get and display articles from database
    décomposition de l'index en header footer main, ajout du logo du site original et d'un favicon

---
+++ blog/articles.php
@@ -0,0 +1,15 @@
+<?php
+  include "header.php"
+ ?>
+<div class="container">
+  <div class="row">
+    <h4>
+      <?php echo $_GET['titre'];  ?>
+    </h4>
+  </div>
+  <div class="row">
+    <p>
+      <?php echo $_GET['contenu']; ?>
+    </p>
+  </div>
+</div>
---
+++ blog/get_Articles.php
@@ -0,0 +1,16 @@
+<?php
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "filrougestream";
+// Requête de connexion a la base de donnée.
+$conn = new PDO("mysql:host={$dbServername};dbname={$dbName}",$dbUsername,$dbPassword);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$conn-> exec("set names utf8");
+
+$sql = $conn->prepare("SELECT * FROM articles");
+// Execute la requête sql et la stock dans la variable $result.
+$sql->execute();
+$resultCheck = $sql->rowCount();
+$row = $sql->fetchAll(PDO::FETCH_OBJ);
+ ?>
